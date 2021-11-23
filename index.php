20485bac7ba3ad60187ef4be9eca4381666e69ab
Hugo <hugo.chabert@laplateforme.io>
2021-11-23 14:53:33 +0100

    Ajout du footer

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require('php/footer.php');
+?>
---
+++ php/footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="../css/footer.css" rel="stylesheet">
+    <title>footer</title>
+</head>
+<body>
+<main>
+
+</main>
+<footer>
+    <div class="footer">
+        <div class="footer1">
+            <a href="https://github.com/hugo-chabert/blog"><img class="socialMedia2"  src="../images/GitHub-Logo.png"></a>
+        </div>
+        <div class="footer2">
+            Copyright © 2021 Hugo, François & Remi. All Rights Reserved
+        </div>
+        <div class="footer3">
+            <a href="https://twitter.com/"><img class="socialMedia"  src="../images/Twitter.png"></a>
+            <a href="https://facebook.com/"><img class="socialMedia" src="../images/Facebook.png"></a>
+            <a href="https://instagram.com/"><img class="socialMedia" src="../images/Instagram.png"></a>
+            <a href="https://youtube.com/"><img class="socialMedia" src="../images/Youtube.png"></a>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
+
