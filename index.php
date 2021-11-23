bda96020b4dcff39981f4d8a2b0f1fd5ace5b84b
remig13 <remi.garguilo@epitech.eu>
2021-11-23 17:43:14 +0100

    ajout de la page connexion et inscription fonctionnel

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    require('php/fonctions/fonctionR.php');
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/index.css" />
+        <title>Acceuil</title>
+    </head>
+<body>
+<header>
+	<a href="php/connexion.php">Connexion</a></li>
+	<a href="php/inscription.php">Inscription</a></li>
+</header>
+<main>
+</main>
+<footer>
+</footer>
+</body>
+</html>
---
+++ php/connexion.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+ require('fonctions/fonctionR.php');
+
+?>
+<html >
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/inscription.css" />
+        <title>Inscription</title>
+    </head>
+<body>
+<header>
+</header>
+<main>
+<div class="login-page">
+  <div class="form">
+    <form action="" method="post" class="login-form">
+        <input type="text" id="login" name="login" placeholder="Login"/>
+        <input type="password" id="password" name="password" placeholder="Password"/>
+        <input type="password" id="Confirmedpassword" name="Confirmedpassword" placeholder="Confirmed Password"/>
+        <input class="button" type="submit" value="Login"/>
+        <p class="message">Not registered ? <a href="inscription.php"><strong>Sign in</strong></a></p>
+    </form>
+    <?php connect_user(); ?>
+  </div>
+</div>
+</form>
+</main>
+<footer>
+</footer>
+</body>
+</html>
