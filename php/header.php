ac35ec36de606ad67364048636c572efc30a97d2
Hugo <hugo.chabert@laplateforme.io>
2021-11-24 15:04:34 +0100

    Fin header

--- php/header.php
+++ php/header.php
@@ -7,7 +7,20 @@
 <body>
 <header>
     <div class="header">
-        
+        <div class="titreHeader">
+            ADDICT X SHOES
+        </div>
+        <div class="links">
+            <div class="gaucheHeader">
+                <a href="<?php if($nomFichier != 'index.php'){ echo '../';} ?>index.php" class="linkHeader">ACCUEIL</a>
+                <a href="" class="linkHeader">CATEGORIES</a>
+            </div>
+            <div class="droiteHeader">
+                <a href="<?php if($nomFichier != 'index.php'){ echo '../';} ?>php/inscription.php" class="linkHeader">INSCRIPTION</a>
+                <a href="<?php if($nomFichier != 'index.php'){ echo '../';} ?>php/connexion.php" class="linkHeader">CONNEXION</a>
+                <a href="<?php if($nomFichier != 'index.php'){ echo '../';} ?>php/profil.php" class="linkHeader"><img class="imgProfil" src="<?php if($nomFichier != 'index.php'){ echo '../';} ?>images/profil.png"></a></a>
+            </div>
+        </div>
     </div>
 </header>
 <main>
