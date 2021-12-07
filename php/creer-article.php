110f250f172d23c4ad11f3cc7e918c652c1202e9
francois-niang1 <francois.niang@laplateforme.io>
2021-12-07 18:09:42 +0100

    CSS Creation d'article Peaufinnage et commencement gestion d'erreur

--- php/creer-article.php
+++ php/creer-article.php
@@ -2,27 +2,40 @@
 $nomFichier = basename (__FILE__);
 session_start();
 require('fonctions/fonctionR.php');
-
 verif_admin_modo();
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Creer Article</title>
-</head>
-<?php require 'header.php';?>
-<body>
-    <form method="post">
-        <input type="text" name="txt_article" placeholder="Créer un Article">
-        <select name="cat">
-            <option value="choose" name="choose">--Choisissez une catégorie d'article à ajouter--</option>
-            <?php  $value = auto_list();?>
-        </select>
-        <input  type="submit" name="envoyer" value="Envoyer"/>
-    </form>
-    <?php  create_article();?>
-</body>
-<?php require 'footer.php';?>
+    <head>
+        <meta charset="UTF-8">
+        <title>Creer Article</title>
+        <link rel="stylesheet" href="../css/creation.css">
+        <link rel="stylesheet" href="../css/root.css">
+        <link rel="stylesheet" href="../css/font.css">
+    </head>
+    <body>
+        <?php require 'header.php';?>
+        <main>
+            <div class="container">
+                <div class="box-form">
+                    <h1>Impressionnez nous en ecrivant votre plus bel article !!</h1>
+                    <form class ='form' method="post">
+                        <input type="text" name="nom_article" placeholder="Titre de l'Article">
+                        <select name="cat">
+                            <option value="choose" name="choose">-Choisissez une catégorie-</option>
+                            <?php  $value = auto_list();?>
+                        </select>
+                        <textarea type="textarea" name="txt_article" placeholder="Description de l'Article"></textarea>
+                        <?php create_article(); ?>
+                        <div class="annuler-submit">
+                            <button class = 'button' type="submit" name="envoyer"> Créer l'article</button>
+                            <button class = 'button'><a href="profil.php"> Annuler</a></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
+    <?php require 'footer.php';?>
+    </body>
 </html>
--- php/header.php
+++ php/header.php
@@ -9,6 +9,7 @@
             <div class="droiteHeader">
                 <?php if(empty($_SESSION['user'])){echo ('<a href="'); if($nomFichier != 'index.php'){ echo '../';} echo ('php/inscription.php" class="linkHeader">INSCRIPTION</a>');} ?>
                 <?php if(empty($_SESSION['user'])){echo ('<a href="'); if($nomFichier != 'index.php'){ echo '../';} echo ('php/connexion.php" class="linkHeader">CONNEXION</a>');} ?>
+                <?php if(!empty($_SESSION['user'])){if($_SESSION['user']['id_droits'] == '1337' || $_SESSION['user']['id_droits'] == '42' ){echo ('<a href="'); if($nomFichier != 'index.php'){ echo '../';} echo ('php/creer-article.php" class="linkHeader">CREER UN ARTICLE</a>');}} ?>
                 <?php if(!empty($_SESSION['user'])){if($_SESSION['user']['id_droits'] == '1337'){echo ('<a href="'); if($nomFichier != 'index.php'){ echo '../';} echo ('php/admin.php" class="linkHeader">ADMIN</a>');}} ?>
                 <?php if(!empty($_SESSION['user'])){echo ('<form action="" method = "POST" class="decoButton"><button class = "deco2" type = "submit" name = "deconnexion" value ="Deconnexion">DECONNEXION</button></form>');} ?>
                 <?php if(!empty($_SESSION['user'])){echo ('<a href="'); if($nomFichier != 'index.php'){ echo '../';} echo ('php/profil.php" class="linkHeader"><img class="imgProfil" src="'); if($nomFichier != "index.php"){ echo "../";}; echo ('images/profil.png"></a></a>');} ?>
