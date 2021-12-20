a85b99746b96dd2cd04b6cc5cb11adc294201de3
francois-niang1 <francois.niang@laplateforme.io>
2021-12-20 16:33:20 +0100

    correction footer

--- php/footer.php
+++ php/footer.php
@@ -2,9 +2,14 @@
 <footer>
     <div class="footer">
         <div class="footer1">
+        Accueil<a href="<?php if($nomFichier == 'index.php'){ echo 'php/';}?>"><img class="socialMedia" alt="Accueil" src="https://cdn-icons-png.flaticon.com/512/1239/1239292.png"></a>
+        Articles<a href="<?php if($nomFichier == 'index.php'){ echo 'php/';}?>"><img class="socialMedia" alt="Articles" src="http://cdn.onlinewebfonts.com/svg/img_351420.png"></a>
+        Connexion<a href="<?php if($nomFichier == 'index.php'){ echo 'php/';}?>"><img class="socialMedia" alt="Connexion" src="http://cdn.onlinewebfonts.com/svg/img_201469.png"></a>
+        Inscription<a href="<?php if($nomFichier == 'index.php'){ echo 'php/';}?>"><img class="socialMedia" alt="Inscription" src="https://www.vbvb.fr/wp-content/uploads/2016/04/icone_sinscrire.png"></a>
+        Profil<a href="<?php if($nomFichier == 'index.php'){ echo 'php/';}?>"><img class="socialMedia" alt="Profil" src="http://cdn.onlinewebfonts.com/svg/img_311846.png"></a>
         </div>
         <div class="footer2">
-            Copyright © 2021 Hugo, François & Remi. All Rights Reserved
+            <h2>Copyright © 2021 Hugo, François & Remi. All Rights Reserved</h2>
         </div>
         <div class="footer3">
             <a href="https://twitter.com/"><img class="socialMedia" alt="Twitter" src="<?php if($nomFichier != 'index.php'){ echo '../';} ?>images/Twitter.png"></a>
